a3c165d0fdbcd941e49c0bdfd18b12e9ec256a41
riazsarker <riaz.cse260@gmail.com>
2023-02-10 23:29:47 +0600

    JS Problem solving

---
+++ js/event.js
@@ -0,0 +1,36 @@
+
+// option 2 add onClick
+function makeRed(){
+    document.body.style.backgroundColor ='red';
+}
+
+// option 3
+const makeBlueButton = document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+        function makeBlue(){
+            document.body.style.backgroundColor ='blue';
+        }
+        // option 3 another
+        const purpleButton = document.getElementById('make-purple');
+        purpleButton.onclick = function makePurple(){
+            document.body.style.backgroundColor = 'purple';
+        }
+
+        // option 4
+        const pinkButton = document.getElementById('make-pink');
+        pinkButton.addEventListener('click',makePink);
+
+        function makePink(){
+            document.body.style.backgroundColor ='pink'
+        }
+
+        // option 4 anther
+        const makeGreenButton = document.getElementById('make-green');
+        makeGreenButton.addEventListener('click', function makeGreen(){
+            document.body.style.backgroundColor='green'
+        })
+
+        // final option 
+        document.getElementById('make-goldenrod').addEventListener('click',function(){
+            document.body.style.backgroundColor='goldenrod'
+        })
